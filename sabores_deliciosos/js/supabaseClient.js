7cab6ee1e4264557fa44e882a2dc34fbc70af879
brolyjod-rgb <brolyjod@gmail.com>
2026-03-17 09:40:35 -0500

    Update supabaseClient.js

--- sabores_deliciosos/js/supabaseClient.js
+++ sabores_deliciosos/js/supabaseClient.js
@@ -1,8 +1,7 @@
 // js/supabaseClient.js
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
-// ⚠️ REEMPLAZAR POR TUS VALORES REALES
-const SUPABASE_URL = "https://ntfthzhhpnstdhjsuavv.supabase.co";
-const SUPABASE_ANON_KEY = "TU_ANON_KEY_AQUI";
+const SUPABASE_URL = "https://ntfthzhhpnstdhjsuavv.supabase.co"; 
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im50ZnRoemhocG5zdGRoanN1YXZ2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM2OTIxMjEsImV4cCI6MjA4OTI2ODEyMX0.P52NLGWOnsu2T2fAjeGDT1Ii1arzJDv1C2I5KnkNEGg";
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
